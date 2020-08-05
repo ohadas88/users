49e16b4ccd0e7a307e298bd2cf50576cb8b77eb9
ohad <ohadas88@gmail.com>
2020-08-05 10:54:50 +0300

    returning countries array

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const usersBtn = $("#getUsers");
 const genderContainer = $("#genderContainer");
+const countriesContainer = $("#countriesContainer");
 usersBtn.on("click", async () => {
   try {
     const result = await getUsers({
       url: `https://randomuser.me/api?results=50`,
     });
     const { results } = result;
-    const gender = genderSelection(results);
-    console.log(gender);
+    const gender = _genderSelection(results);
+    const countries = _countriesSelection(results);
+    _draw(gender, countries);
   } catch (error) {
     alert("ERROR");
   }
 
-  function genderSelection(usersArray) {
+  function _genderSelection(usersArray) {
     const genderSelection = usersArray.map((user) => user.gender);
     const maleStatistic = genderSelection.filter(
       (userGender) => userGender === "male"
@@ -28,5 +30,38 @@ usersBtn.on("click", async () => {
       male: $(maleStatistic).length * percentagePerUser,
       female: $(femaleStatistic).length * percentagePerUser,
     };
+  }
+
+  function _countriesSelection(usersArray) {
+    const countries = usersArray.reduce((obj, user) => {
+      const { country } = user.location;
+      const count = obj[country] || 0;
+      return { ...obj, [country]: count + 1 };
+    }, {});
+
+    const countryKeyVal = Object.entries(countries);
+constcountryResult= countryKeyVal.forEach(countryArray => {
+    
+});
+
+    return countryKeyVal;
+  }
+
+  function _draw(genderPercentages, countriesObj) {
+    const genderTable = $(`<table>
+    <tr>
+    <th>Male</th>
+    <th>Female</th>
+    </tr>
+    <tr>
+    <td>${genderPercentages.male}%</td>
+    <td>${genderPercentages.female}%</td>
+    </tr>
+    
+  </table>
+  `);
+    console.log(countriesObj);
+
+    genderContainer.append(genderTable);
   }
 });
